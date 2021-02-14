f4c4eaa496a214f89197743930bc49ec30c6b29a
ken025 <kennethmanuel250@gmail.com>
2021-02-13 21:51:39 -0500

    ran npm build and deployed app with surge

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -22,7 +22,7 @@ const AboutMe = () => {
                         I build projects on my own time where I focus on making them useful for real-world situations and expandable so that I can constantly maintain them. 
                         This way, as my skills grow, so do my projects.
                     </h5><br/>
-                    <Button href="https://docs.google.com/document/d/1yJGRuDhLW-mOsPDwK2OG3Z3KJOmDNOxJ7l319snWUWI/edit?usp=sharing" target="_blank"> <AiOutlineFileDone size={30}/>My Resumé </Button><br/>
+                    <Button href="https://docs.google.com/document/d/19mDTJ5vVL6T5XCwa1vrD6e7IXR9-XkRfF_EirVpKzBk/edit?usp=sharing" target="_blank"> <AiOutlineFileDone size={30}/>My Resumé </Button><br/>
                 </Col>
             </Row>
         </Container>
