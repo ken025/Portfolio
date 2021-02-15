d378eaece22801881de1dc2eb1f5bda378161bc0
ken025 <kennethmanuel250@gmail.com>
2021-02-14 23:06:11 -0500

    added background to about me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -14,7 +14,7 @@ const AboutMe = () => {
                     <Image src="https://scontent.ftpa1-2.fna.fbcdn.net/v/t1.0-9/s1080x2048/142665065_1783935365109746_9053597600276630280_o.jpg?_nc_cat=102&ccb=2&_nc_sid=730e14&_nc_ohc=rL8JV3p-IV4AX-yGfDJ&_nc_ht=scontent.ftpa1-2.fna&tp=7&oh=f39322d72c13f7f73184f18758e57703&oe=60338594" thumbnail roundedCircle />
                 </Col>
                 <Col xs={6} md={10}>
-                    <h5>
+                    <h5 className="abt">
                         I am a Full-Stack Software Engineer. 
                         For as long as I can remember I have always had a passion for the world of technology, creating and innovating.
                         Software Engineering was the perfect mix of all of these, and so I decided to follow my dreams.  
